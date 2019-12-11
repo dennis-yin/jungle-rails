4c881d6667ef0aaf5c9ae27c7c4ca21ea8c8a86c
dennis-yin <dennis.yin26@gmail.com>
2019-12-11 13:42:32 -0800

    add stats to admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
   def show
+    @num_products = Product.count(:id)
+    @num_categories = Category.count(:id) 
   end
+
 end
